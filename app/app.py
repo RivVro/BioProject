522aef6659331768fdea54b49f9aefc5f040ec53
RivVro <31916510+RivVro@users.noreply.github.com>
2019-06-11 18:43:13 +0200

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,29 +1,42 @@
-from flask import Flask
-import vcf
+from typing import List, Dict
+from flask import Flask, request
+import mysql.connector
 import json
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 
-#@app.route('/')
-def lees_bestand():
-    vcf_reader = vcf.Reader(open('gnomad.exomes.r2.1.1.sites.Y.vcf', 'r'))
-    open("LijstSNP.txt", "w").close()
-    lijst = []
-    for record in vcf_reader:
-        try:
-            #print(record)
-            TXTinput = open("LijstSNP.txt", "a+")
-            db_Data = '(\''+str(record.CHROM) +'\',\''+ str(record.POS) +'\',\''+ str(record.REF) +'\',\''+ str(record.ALT).replace("[", "").replace("]","") + '\',\''+ str(record.INFO['AF'][0]) + '\'),\n'
-            TXTinput.write(db_Data)
-            TXTinput.close
-        except KeyError:
-            continue
+def variants(chrom, pos, aft):
+#connect met de database
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'gnomad'
+    }
 
-def main():
-    lees_bestand()
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+#query die zoekt naar varianten die de chromosoom, positie en alternative van de user input bevatten.
+    query = 'SELECT * FROM variants WHERE chrom=%s AND pos=%s AND aft=%s AND freq<0.01;'
+    cursor.execute(query, (chrom,pos,aft))
+    results = cursor.fetchone()
+    cursor.close()
+    connection.close()
+#Wanneer de opgevraagde positie niet in de database is gevonden vind er een print plaats
+    if results == none:
+        print('deze positie is niet benign of bestaat niet in de database')
+    return results
 
+#api die de user input meegeeft
+@app.route('/')
+def index():
+    chrom = request.args.get('chrom')
+    pos =  request.args.get('pos')
+    aft =  request.args.get('aft')
+    results = variants(chrom, pos, aft)
+    return str(results)
 
-main()
-#if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
