9f36c4821b767ca1624501224c9ce1bce57ad88f
RivVro <31916510+RivVro@users.noreply.github.com>
2019-07-01 15:39:18 +0200

    Add files via upload

--- dataGnomad.py
+++ dataGnomad.py
@@ -1,13 +1,15 @@
 import vcf
 
+
 #Leest vcf bestand met SNP-varianten. Schrijft chromosoom, positie, variantie en frequentie weg in teksbestand dat als input voor database gebruikt wordt.
+
 def lees_bestand():
-    vcf_reader = vcf.Reader(open('gnomad.exomes.r2.1.1.sites.Y.vcf', 'r'))
+    vcf_reader = vcf.Reader(open('C:/Users/rivvr/OneDrive/Documenten/BioPredict/gnomad.exomes.r2.1.1.sites.Y.vcf', 'r'))
     open("LijstSNP.txt", "w").close()
     for record in vcf_reader:
         try:
             TXTinput = open("LijstSNP.txt", "a+")
-            db_Data = '(\''+str(record.CHROM) +'\',\''+ str(record.POS) +'\',\''+ str(record.REF) +'\',\''+ str(record.ALT).replace("[", "").replace("]","") + '\',\''+ str(record.INFO['AF'][0]) + '\'),\n'
+            db_Data = '(\''+str(record.CHROM) +'\','+ str(record.POS) +',\''+ str(record.REF) +'\',\''+ str(record.ALT).replace("[", "").replace("]","") + '\','+ str(record.INFO['AF'][0]) + '),\n'
             TXTinput.write(db_Data)
             TXTinput.close
         except KeyError:
@@ -17,4 +19,3 @@ def main():
     lees_bestand()
 
 main()
-
