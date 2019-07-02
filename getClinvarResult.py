7f54c32629ae4b93a4c4681dc52a6c843ae41eee
travelanywhere <nickyvanbergen@live.nl>
2019-07-02 13:40:57 +0200

    clinvar verder uitgebreid

--- getClinvarResult.py
+++ getClinvarResult.py
@@ -7,8 +7,14 @@ Created on Mon Jul  1 20:56:40 2019
 import myvariant
 mv = myvariant.MyVariantInfo()
 clinvar_result = mv.getvariant("chr7:g.140453134T>C")
-str1 = str(clinvar_result)
-f = open("test.txt","w") #opens file with name of "test.txt"
-f.write(str1)
-f.close()
-print("gelukt")
+text = str(clinvar_result)
+file = open("test.txt","w") #opens file with name of "test.txt"
+# if statement variabele not null
+if text != None:
+  file.write("No results found on Clinvar")
+else:
+  file.write(text)
+file.close()
+with open('test.txt', 'r') as f2:
+    data = f2.read()
+    print(data)
